e1282cb549add60e05f3fc257a85e6502aa1651a
AssassinLYZ <goatherd.eric@gmail.com>
2024-04-21 12:50:44 +0200

    fix:fix deploy problem

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
-import HelloWorld from '../../layout/HelloWorld.vue'
+import Layout from '@/layout/LayoutView.vue'
 
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
+    const wrapper = mount(Layout, { props: { msg: 'Hello Vitest' } })
+    expect(wrapper.text()).toContain('This is the table for Shireburn Assignment')
   })
 })
